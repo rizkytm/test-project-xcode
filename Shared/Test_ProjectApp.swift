7d8831aaf2e6cd0872be32b9046b6b1e204620ae
rizkytm <ritrime97@gmail.com>
2022-04-06 13:49:00 +0700

    first commit

---
+++ Shared/Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProjectApp.swift
+//  Shared
+//
+//  Created by Aurelia Trixi on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
